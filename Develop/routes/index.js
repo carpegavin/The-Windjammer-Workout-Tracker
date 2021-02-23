9380bc4f4eb3a9ed3ce47c66ec0269774dccb16d
Gavin Phill <carpe.gavin@gmail.com>
2021-02-23 17:06:56 -0600

    add get/post/put workout requests in routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,12 +1,47 @@
 const path = require("path");
 const router = require("express").Router();
 const Workout = require("../models");
+const id = req.params.id;
+const body = req.body;
 
 // get workouts
+app.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// post workouts 
+// put workouts 
+app.put("/api/workouts", ({body}, res) => {
+    db.Workout.create(body)
+      .then(({_id}) => db.Workout.findOneAndUpdate({}, 
+        { $push: { exercises: body } }, 
+        { new: true }))
+      .then(data) => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
-// put workouts
+// post workouts
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+
 
 // html routes
 // get from all three html pages
