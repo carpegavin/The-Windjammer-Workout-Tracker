e21518ea5d0a404210c4e1dcc5e1373f00fa3f2b
Gavin Phill <carpe.gavin@gmail.com>
2021-02-23 15:54:30 -0600

    pseudocode route and workout.js

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -2,80 +2,12 @@ const path = require("path");
 const router = require("express").Router();
 const Workout = require("../models");
 
-//API Routes
-router.get("/api/workouts", (req, res) => {
-  Workout.Workout.find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+// get workouts
 
-router.post("/api/workouts", (req, res) => {
-  Workout.Workout.create({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+// post workouts 
 
-router.put("/api/workouts/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
+// put workouts
 
-  Workout.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.put("/api/workouts/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-
-  Workout.Workout.findOneAndUpdate(
-    { _id: id },
-    { $push: { exercises: body } },
-    { new: true, runValidators: true }
-  )
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/workouts/range", (req, res) => {
-  Workout.Workout.find({})
-    .limit(7)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-//HTML Routes
-router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
-
-router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-module.exports = router;
-
+// html routes
+// get from all three html pages
+// stats, index, exercise
