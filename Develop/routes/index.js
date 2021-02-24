6000fb31a7cabb98aedba42f836c04dc9d581bd0
Gavin Phill <carpe.gavin@gmail.com>
2021-02-23 18:25:32 -0600

    add HTML routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,32 +3,42 @@ const router = require("express").Router();
 const Workout = require("../models");
 const id = req.params.id;
 const body = req.body;
+const db = require("../models")
+
+module.exports = app;
 
 // get workouts
 app.get("/api/workouts", (req, res) => {
-  db.Workout.aggregate({})
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// put workouts 
-app.put("/api/workouts", ({body}, res) => {
-    db.Workout.create(body)
-      .then(({_id}) => db.Workout.findOneAndUpdate({}, 
-        { $push: { exercises: body } }, 
-        { new: true }))
-      .then(data) => {
-        res.json(data);
+  db.Workout.aggregate([{
+      $addFields:{
+          "totalDuration":{
+              $sum: "$exercises.duration"
+          }
+      }
+  }],
+  (err,data)=>{
+      if(err){res.send(error)
+      }
+       else{console.log(data)
+          res.send(data)}
       })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  })
 
+
+  // put workouts 
+app.put("/api/workouts/:id", ({body}, res) => {
+    db.Workout.findOneAndUpdate (
+        req.params.id,{
+            $push: { exercises: body }
+        },
+        (err,data)=>{
+            if(err){res.send(error)
+            }
+            else{console.log(data)
+            res.send(data)}
+        }
+    );
+    },
 // post workouts
 
 app.post("/api/workouts", (req, res) => {
@@ -39,10 +49,22 @@ app.post("/api/workouts", (req, res) => {
       .catch(err => {
         res.json(err);
       });
-  });
+  }));
 
 
 
 // html routes
 // get from all three html pages
 // stats, index, exercise
+
+app.get("/stats", (req, res) => {
+    re.send("/public/stats.html");
+});
+
+app.get("/exercise", (req, res) => {
+    re.send("/public/exercise.html");
+});
+
+app.get("/index", (req, res) => {
+    re.send("/public/index.html");
+});
--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,22 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+const db = require("./models");
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
-app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+    console.log(`App running on port ${PORT}!`);
+  });
