62a432133fbe043f89a5bd4fac910480f508ddb2
Gavin Phill <carpe.gavin@gmail.com>
2021-02-26 12:40:32 -0600

    added process/env in seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
